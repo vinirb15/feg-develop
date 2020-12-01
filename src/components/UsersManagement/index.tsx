37f4cc9485f10f78dc620afee3704b2c31cc4f51
vbueno <viniciusbueno15@gmail.com>
2020-12-01 19:10:43 -0300

    Update User with Modal

--- src/components/UsersManagement/index.tsx
+++ src/components/UsersManagement/index.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiFilePlus, FiEdit, FiTrash, FiToggleLeft } from 'react-icons/fi';
+import { Modal } from '@material-ui/core'
 
 import Image from '../../assets/Monograma.png';
 
 import './styles.css';
 
 const Management = () => {
+    const [users, setUsers] = useState([
+        {
+            ID: "1",
+            USER: "Alfreds Futterkiste",
+            LOCATION: "Germany",
+            EMAIL: "alfredsfutterkiste@gmail.com",
+            GROUP: "FEG",
+            ACTIVED: false
+        },
+        {
+            ID: "2",
+            USER: "Futterkiste Alfreds",
+            LOCATION: "Germany",
+            EMAIL: "futterkistealfreds@gmail.com",
+            GROUP: "FEG",
+            ACTIVED: true
+        }
+    ]);
+
+    const [modal, setModal] = useState(false)
+
+    function handleDeleteUser(event: string) {
+        try {
+            setUsers(users.filter(user => user.ID !== event));
+        } catch (error) {
+            alert('Error')
+        }
+    }
 
     return (
         <div className="management-content">
@@ -13,7 +42,8 @@ const Management = () => {
             <div className="management-top">
 
                 <div className="top-left">
-                    <h1>Users <b>480 total</b></h1>
+                    <h1>Users</h1>
+                    <h2> 480 total</h2>
                     <input type="text" placeholder="Search..." />
                 </div>
 
@@ -30,7 +60,7 @@ const Management = () => {
             <div className="users-management">
                 <div className="list-header">
                     <h2>User Management</h2>
-                    <div>
+                    <div className="export">
                         <select name="">
                             <option value="">Export</option>
                             <option value="">Option</option>
@@ -43,7 +73,6 @@ const Management = () => {
 
                 <table className="users">
                     <tr>
-                        <th>ID</th>
                         <th id="image"></th>
                         <th>USER</th>
                         <th>LOCATION</th>
@@ -52,38 +81,69 @@ const Management = () => {
                         <th>STATUS</th>
                         <th>ACTIONS</th>
                     </tr>
-                    <tr>
-                        <td>1</td>
-                        <td id="image">
-                            <img src={Image} alt="user.png" />
-                        </td>
-                        <td >Alfreds Futterkiste</td>
-                        <td>Germany</td>
-                        <td>alfredsfutterkiste@gmail.com</td>
-                        <td>Group Here</td>
-                        <td><b style={{ background: '#808080' }}>Inactive</b></td>
-                        <td>
-                            <button><FiToggleLeft color='#808080' /></button>
-                            <button><FiEdit color='#808080' /></button>
-                            <button><FiTrash color='#808080' /></button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td id="image">
-                            <img src={Image} alt="user.png" />
-                        </td>
-                        <td >Alfreds Futterkiste</td>
-                        <td>Germany</td>
-                        <td>alfredsfutterkiste@gmail.com</td>
-                        <td>Group Here</td>
-                        <td><b style={{ background: '#25ab9f' }}>Active</b></td>
-                        <td>
-                            <button><FiToggleLeft color='#808080' /></button>
-                            <button><FiEdit color='#808080' /></button>
-                            <button><FiTrash color='#808080' /></button>
-                        </td>
-                    </tr>
+
+
+                    {
+                        users.map(user => (
+                            <tr key={user.ID}>
+                                <td id="image">
+                                    <img src={Image} alt="user.png" />
+                                </td>
+                                <td><p className="label-mobile">USER:</p> {user.USER}</td>
+                                <td><p className="label-mobile">LOCATION:</p> {user.LOCATION}</td>
+                                <td><p className="label-mobile">EMAIL:</p> {user.EMAIL}</td>
+                                <td><p className="label-mobile">GROUP:</p> {user.GROUP}</td>
+                                <td><p className="label-mobile">STATUS:</p> <b style={{ background: user.ACTIVED ? "#25ab9f" : "#808080" }}>{user.ACTIVED ? 'ACTIVED' : 'INATIVED'}</b></td>
+                                <td>
+                                    <button><FiToggleLeft color='#808080' /></button>
+                                    <button onClick={() => setModal(true)} ><FiEdit color='#808080' /></button>
+                                    <button><FiTrash onClick={() => handleDeleteUser(user.ID)} color='#808080' /></button>
+                                </td>
+                                <div id="myModal" style={{ display: modal ? "block" : "none" }} className="modal">
+                                    <div className="modal-content">
+                                        <div className="modal-header">
+                                            <span onClick={() => setModal(false)} className="close">&times;</span>
+                                            <h2>Update User</h2>
+                                        </div>
+                                        <div className="modal-content">
+                                            <form onSubmit={() => handleDeleteUser}>
+                                                <h2>Name:</h2>
+                                                <input
+                                                    type="text"
+                                                    placeholder={user.USER}
+                                                // value={user.EMAIL}
+                                                // onChange={e => setName(e.target.value)}
+                                                />
+                                                <h2>Location:</h2>
+                                                <input
+                                                    type="text"
+                                                    placeholder={user.LOCATION}
+                                                // value={user.EMAIL}
+                                                // onChange={e => setName(e.target.value)}
+                                                />
+                                                <h2>Email:</h2>
+                                                <input
+                                                    type="email"
+                                                    placeholder={user.EMAIL}
+                                                // value={user.EMAIL}
+                                                // onChange={e => setEmail(e.target.value)}
+                                                />
+                                                <h2>Group:</h2>
+                                                <input
+                                                    type="text"
+                                                    placeholder={user.GROUP}
+                                                // value={user.EMAIL}
+                                                // onChange={e => setName(e.target.value)}
+                                                />
+
+                                                <button className="button" type="submit">Update</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </tr>
+                        ))
+                    }
                 </table>
             </div>
 

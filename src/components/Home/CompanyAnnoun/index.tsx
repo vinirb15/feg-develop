038936506165b48e49bdf084f4d70875d3b2f11c
vbueno <viniciusbueno15@gmail.com>
2020-12-02 19:38:09 -0300

    Delete Users

--- src/components/Home/CompanyAnnoun/index.tsx
+++ src/components/Home/CompanyAnnoun/index.tsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 
 import './styles.css';
 
+import Announcement from '../../../assets/Announcements/ScreenShot1.png'
+
 const Content = () => {
 
     return (
@@ -10,6 +12,15 @@ const Content = () => {
             <Link to="/">
                 <h1>Company Announcements</h1>
             </Link>
+            <p>11.30.2020 - 3:45PM</p>
+            <div className="content">
+            <img src={Announcement} alt="Announcement" />
+            <h2 className="text">Good afternoon everyone, as you all know this year has been a doozy.
+            It's imperative that we as a whole are watching our spending by being overly cautious when ordering
+            and utilizing any items you have in house.
+                </h2>
+            <button>See more...</button>
+            </div>
         </div>
     );
 }
--- src/components/UsersManagement/index.tsx
+++ src/components/UsersManagement/index.tsx
@@ -1,36 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FiEdit, FiTrash, FiToggleLeft } from 'react-icons/fi';
+import axios from 'axios';
 
 import Image from '../../assets/Monograma.png';
 
 import './styles.css';
 
+
 const Management = () => {
     const [users, setUsers] = useState([
         {
-            ID: "1",
-            USER: "Alfreds Futterkiste",
-            LOCATION: "Germany",
-            EMAIL: "alfredsfutterkiste@gmail.com",
-            GROUP: "FEG",
-            ACTIVED: false
-        },
-        {
-            ID: "2",
-            USER: "Futterkiste Alfreds",
-            LOCATION: "Germany",
-            EMAIL: "futterkistealfreds@gmail.com",
-            GROUP: "FEG",
-            ACTIVED: true
+            id: "",
+            firstName: "",
+            lastName: "",
+            location_name: "",
+            email: "",
+            profile: "",
+            isActive: false
         }
     ]);
 
+    useEffect(() => {
+        handleLoad()
+    }, [])
+
+    async function handleLoad() {
+        const response: any = await axios.get(`http://127.0.0.1:4200/api/v1/accounts`)
+        console.log(response.data);
+        setUsers(response.data)
+    }
+
+
     const [modal, setModal] = useState(false)
     const [confirmation, setConfirmation] = useState(false)
+    const [modalDate, setModalDate] = useState<any>()
 
-    function handleDeleteUser(event: string) {
+    async function handleDeleteUser(id: string) {
         try {
-            setUsers(users.filter(user => user.ID !== event));
+            await axios.delete(`http://127.0.0.1:4200/api/v1/accounts/${id}`)
+            setUsers(users.filter(user => user.id !== id));
             setConfirmation(false)
         } catch (error) {
             alert('Error')
@@ -83,87 +91,80 @@ const Management = () => {
                     </tr>
 
 
-                    {
-                        users.map(user => (
-                            <tr key={user.ID}>
-                                <td id="image">
-                                    <img src={Image} alt="user.png" />
-                                </td>
-                                <td><p className="label-mobile">USER:</p> {user.USER}</td>
-                                <td><p className="label-mobile">LOCATION:</p> {user.LOCATION}</td>
-                                <td><p className="label-mobile">EMAIL:</p> {user.EMAIL}</td>
-                                <td><p className="label-mobile">GROUP:</p> {user.GROUP}</td>
-                                <td><p className="label-mobile">STATUS:</p> <b style={{ background: user.ACTIVED ? "#25ab9f" : "#808080" }}>{user.ACTIVED ? 'ACTIVED' : 'INATIVED'}</b></td>
-                                <td>
-                                    <button><FiToggleLeft color='#808080' /></button>
-                                    <button onClick={() => setModal(true)} ><FiEdit color='#808080' /></button>
-                                    <button><FiTrash onClick={() => setConfirmation(true)} color='#808080' /></button>
-                                </td>
-                                <div id="myModal" style={{ display: modal ? "block" : "none" }} className="modal">
+                    {users.map(user => (
+                        <tr key={user.id}>
+                            <td id="image">
+                                <img src={Image} alt="user.png" />
+                            </td>
+                            <td><p className="label-mobile">USER:</p> {user.firstName} {user.lastName}</td>
+                            <td><p className="label-mobile">LOCATION:</p> {user.location_name}</td>
+                            <td><p className="label-mobile">EMAIL:</p> {user.email}</td>
+                            <td><p className="label-mobile">GROUP:</p> {user.profile}</td>
+                            <td><p className="label-mobile">STATUS:</p> <b style={{ background: user.isActive ? "#25ab9f" : "#808080" }}>{user.isActive ? 'ACTIVED' : 'INATIVED'}</b></td>
+                            <td>
+                                <button><FiToggleLeft color='#808080' /></button>
+                                <button onClick={() => setModal(true)} ><FiEdit color='#808080' /></button>
+                                <button><FiTrash onClick={() => { setConfirmation(true); setModalDate(user.id) }} color='#808080' /></button>
+                            </td>
+                            <div id="myModal" style={{ display: modal ? "block" : "none" }} className="modal">
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                        <span onClick={() => setModal(false)} className="close">&times;</span>
+                                        <h2>Update User</h2>
+                                    </div>
                                     <div className="modal-content">
-                                        <div className="modal-header">
-                                            <span onClick={() => setModal(false)} className="close">&times;</span>
-                                            <h2>Update User</h2>
-                                        </div>
-                                        <div className="modal-content">
-                                            <form onSubmit={() => handleDeleteUser}>
-                                                <h2>Name:</h2>
-                                                <input
-                                                    type="text"
-                                                    placeholder={user.USER}
-                                                // value={user.EMAIL}
-                                                // onChange={e => setName(e.target.value)}
-                                                />
-                                                <h2>Location:</h2>
-                                                <input
-                                                    type="text"
-                                                    placeholder={user.LOCATION}
-                                                // value={user.EMAIL}
-                                                // onChange={e => setName(e.target.value)}
-                                                />
-                                                <h2>Email:</h2>
-                                                <input
-                                                    type="email"
-                                                    placeholder={user.EMAIL}
-                                                // value={user.EMAIL}
-                                                // onChange={e => setEmail(e.target.value)}
-                                                />
-                                                <h2>Group:</h2>
-                                                <input
-                                                    type="text"
-                                                    placeholder={user.GROUP}
-                                                // value={user.EMAIL}
-                                                // onChange={e => setName(e.target.value)}
-                                                />
+                                        <form onSubmit={() => handleDeleteUser}>
+                                            <h2>Name:</h2>
+                                            <input
+                                                type="text"
+                                                placeholder={user.firstName + user.lastName}
+                                            // value={user.EMAIL}
+                                            // onChange={e => setName(e.target.value)}
+                                            />
+                                            <h2>Location:</h2>
+                                            <input
+                                                type="text"
+                                                placeholder={user.location_name}
+                                            // value={user.EMAIL}
+                                            // onChange={e => setName(e.target.value)}
+                                            />
+                                            <h2>Email:</h2>
+                                            <input
+                                                type="email"
+                                                placeholder={user.email}
+                                            // value={user.EMAIL}
+                                            // onChange={e => setEmail(e.target.value)}
+                                            />
+                                            <h2>Group:</h2>
+                                            <input
+                                                type="text"
+                                                placeholder={user.location_name}
+                                            // value={user.EMAIL}
+                                            // onChange={e => setName(e.target.value)}
+                                            />
 
-                                                <button className="button" type="submit">Update</button>
-                                            </form>
-                                        </div>
+                                            <button className="button" type="submit">Update</button>
+                                        </form>
                                     </div>
                                 </div>
+                            </div>
 
-
-
-                                <div id="myModal" style={{ display: confirmation ? "block" : "none" }} className="modal">
-                                    <div className="modal-content">
-                                        <div className="modal-header">
-                                            <span onClick={() => setConfirmation(false)} className="close">&times;</span>
-                                            <h2>Are you sure you want to continue?</h2>
-                                        </div>
-                                        <div className="modal-confirmation">
-                                            <form onSubmit={() => handleDeleteUser}>
-                                                <button type="button" onClick={() => setConfirmation(false)} className="cancelbtn">Cancel</button>
-                                                <button type="button" onClick={() => handleDeleteUser(user.ID)} className="deletebtn">Delete</button>
-                                            </form>
-                                        </div>
+                            <div id="myModal" style={{ display: confirmation ? "block" : "none" }} className="modal">
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                        <span onClick={() => setConfirmation(false)} className="close">&times;</span>
+                                        <h2>Are you sure you want to continue?</h2>
+                                    </div>
+                                    <div className="modal-confirmation">
+                                        <form>
+                                            <button type="button" onClick={() => setConfirmation(false)} className="cancelbtn">Cancel</button>
+                                            <button type="button" onClick={() => handleDeleteUser(modalDate)} className="deletebtn">Delete</button>
+                                        </form>
                                     </div>
                                 </div>
-
-
-
-                            </tr>
-                        ))
-                    }
+                            </div>
+                        </tr>
+                    ))}
                 </table>
             </div>
 

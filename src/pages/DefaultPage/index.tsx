fff91720432d5f0815880f5f47d51171943279db
vbueno <viniciusbueno15@gmail.com>
2020-12-15 19:21:25 -0300

    [feat]: default page

--- src/pages/DefaultPage/index.tsx
+++ src/pages/DefaultPage/index.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import Loader from '../../components/Loader'
 
 import './styles.css';
 
 const HomePage: React.FC = () => {
+    const [loaded, setLoaded] = useState<boolean>(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoaded(false);
+        }, 2000);
+    }, []);
 
     return (
-        <>
-            <div id="notfound">
-                <div className="notfound">
-                    <div className="notfound-404">
-                        <h1>Oops!</h1>
-                        <h2>404 - The Page can't be found</h2>
-                    </div>
-                    <a href="https://account.systemfeg.com">Go TO Homepage</a>
-                </div>
+        loaded ?
+            <div style={{marginTop: "40vh"}}>
+                <Loader />
             </div>
-        </>
+            :
+            <>
+                <div id="notfound">
+                    <div className="notfound">
+                        <div className="notfound-404">
+                            <h1>Oops!</h1>
+                            <h2>404 - The Page can't be found</h2>
+                        </div>
+                        <a href="https://account.systemfeg.com">Go To Homepage</a>
+                    </div>
+                </div>
+            </>
     );
 }
 

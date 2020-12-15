1ca8f5545fbbd00e3554893866150322f8b034f2
vbueno <viniciusbueno15@gmail.com>
2020-12-15 17:23:29 -0300

    [fix]: mouckup changes 1.2

--- src/components/UsersManagement/index.tsx
+++ src/components/UsersManagement/index.tsx
@@ -10,14 +10,21 @@ import Loader from '../../components/Loader';
 const Management = () => {
     const [users, setUsers] = useState([
         {
-            id: "",
-            firstName: "",
-            lastName: "",
-            location_name: "",
-            location_id: "",
+            company_name: "",
+            createdAt: "",
             email: "",
+            firstName: "",
+            group_id: "",
             group_name: "",
-            status: "ACTIVED"
+            id: "",
+            ip: "",
+            lastName: "",
+            location_id: "",
+            location_name: "",
+            password: "",
+            phone_number: "",
+            profile_id: "",
+            status: "",
         }
     ]);
 
@@ -41,9 +48,9 @@ const Management = () => {
     }, [])
 
     async function handleLoad() {
-        const response: any = await axios.get(`https://api-systemfegllc.herokuapp.com/api/v1/accounts`)
+        const response: any = await axios.get(`http://3.130.116.57:4200/api/v1/accounts`)
         setUsers(response.data.results)
-        console.log(response.data.results)
+        console.log(response.data)
         setTotal(response.data.size)
         setLoading(false)
     }
@@ -92,8 +99,8 @@ const Management = () => {
         }
     }
 
-    async function handleActived(actived: string, id: string) {
-        if (actived === 'ACTIVED') {
+    async function handleActived(actived: any, id: string) {
+        if (actived === true) {
             try {
                 await axios.put(`https://api-systemfegllc.herokuapp.com/api/v1/accounts/${id}/blocker`);
 
@@ -116,6 +123,18 @@ const Management = () => {
         }
     }
 
+
+    function parser(actived: string) {
+        if (actived === "ACTIVE") {
+            return true
+        }
+        else if (actived === "INACTIVE") {
+            return false
+        }
+    }
+
+
+
     const table = (
         <div className="management-content">
 
@@ -123,18 +142,8 @@ const Management = () => {
 
                 <div className="top-left">
                     <h1>Users<b> {total} total</b></h1>
-                    {/* <input type="text" placeholder="Search..." /> */}
                 </div>
-
-                {/* <div className="top-right">
-                    <button>Add User</button>
-                    <button>
-                        <FiFilePlus size={25} />
-                    </button>
-                </div> */}
             </div>
-
-
 
             <div className="users-management">
                 <div className="list-header">
@@ -172,74 +181,74 @@ const Management = () => {
                                 <td><p className="label-mobile">LOCATION:</p> {user.location_name}</td>
                                 <td><p className="label-mobile">EMAIL:</p> {user.email}</td>
                                 <td><p className="label-mobile">GROUP:</p> {user.group_name}</td>
-                                <td><p className="label-mobile">STATUS:</p> <b style={{ background: user.status = "ACTIVED" ? "#25ab9f" : "#808080" }}>{user.status = "ACTIVED" ? 'ACTIVED' : 'INATIVED'}</b></td>
+                                <td><p className="label-mobile">STATUS:</p> <b style={{ background: parser(user.status) ? "#25ab9f" : "#808080" }}>{parser(user.status) ? 'ACTIVED' : 'INATIVED'}</b></td>
                                 <td>
-                                    <button><FiToggleLeft onClick={() => handleActived(user.status, user.id)} color='#808080' /></button>
+                                    <button><FiToggleLeft onClick={() => handleActived(parser(user.status), user.id)} color='#808080' /></button>
                                     <button onClick={() => { setModal(true); setModalDate(user) }} ><FiEdit color='#808080' /></button>
                                     <button><FiTrash onClick={() => { setConfirmation(true); setModalDate(user.id) }} color='#808080' /></button>
                                 </td>
-                                <div id="myModal" style={{ display: modal ? "block" : "none" }} className="modal">
-                                    <div className="modal-content">
-                                        <div className="modal-header">
-                                            <span onClick={() => setModal(false)} className="close">&times;</span>
-                                            <h2>Update User</h2>
-                                        </div>
-                                        <div className="modal-content">
-                                            <form onSubmit={() => handleDeleteUser}>
-                                                <h2>Name:</h2>
-                                                <input
-                                                    type="text"
-                                                    placeholder={modalDate ? modalDate.firstName : ""}
-                                                    onChange={e => setName(e.target.value)}
-                                                />
-                                                <h2>Location:</h2>
-                                                <div className="export">
-                                                    <select onChange={e => setLocation(e.target.value)}>
-                                                        <option value={modalDate ? modalDate.location_id : ""}>{modalDate ? modalDate.location_name : ""}</option>
-                                                        <option value="Location 01">Location 01</option>
-                                                        <option value="Location 02">Location 02</option>
-                                                        <option value="Location 03">Location 03</option>
-                                                    </select>
-                                                </div>
-                                                <h2>Email:</h2>
-                                                <input
-                                                    type="email"
-                                                    placeholder={modalDate ? modalDate.email : ""}
-                                                    onChange={e => setEmail(e.target.value)}
-                                                />
-                                                <h2>Group:</h2>
-                                                <div className="export">
-                                                    <select onChange={e => setGroup(e.target.value)}>
-                                                        <option value={modalDate ? modalDate.group_name : ""}>{modalDate ? modalDate.group_name : ""}</option>
-                                                        <option value="Group 01">Group 01</option>
-                                                        <option value="Group 02">Group 02</option>
-                                                        <option value="Group 03">Group 03</option>
-                                                    </select>
-                                                </div>
-
-                                                <button className="button" onClick={() => handleUpdate(modalDate.id)} type="button">Update</button>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div id="myModal" style={{ display: confirmation ? "block" : "none" }} className="modal">
-                                    <div className="modal-content">
-                                        <div className="modal-header">
-                                            <span onClick={() => setConfirmation(false)} className="close">&times;</span>
-                                            <h2>Are you sure you want to continue?</h2>
-                                        </div>
-                                        <div className="modal-confirmation">
-                                            <form>
-                                                <button type="button" onClick={() => setConfirmation(false)} className="cancelbtn">Cancel</button>
-                                                <button type="button" onClick={() => handleDeleteUser(modalDate)} className="deletebtn">Delete</button>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
                             </tr>
                         ))}
                     </tbody>
                 </table>
+                <div id="myModal" style={{ display: modal ? "block" : "none" }} className="modal">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <span onClick={() => setModal(false)} className="close">&times;</span>
+                            <h2>Update User</h2>
+                        </div>
+                        <div className="modal-content">
+                            <form onSubmit={() => handleDeleteUser}>
+                                <h2>Name:</h2>
+                                <input
+                                    type="text"
+                                    placeholder={modalDate ? modalDate.firstName : ""}
+                                    onChange={e => setName(e.target.value)}
+                                />
+                                <h2>Location:</h2>
+                                <div className="export">
+                                    <select onChange={e => setLocation(e.target.value)}>
+                                        <option value={modalDate ? modalDate.location_id : ""}>{modalDate ? modalDate.location_name : ""}</option>
+                                        <option value="Location 01">Location 01</option>
+                                        <option value="Location 02">Location 02</option>
+                                        <option value="Location 03">Location 03</option>
+                                    </select>
+                                </div>
+                                <h2>Email:</h2>
+                                <input
+                                    type="email"
+                                    placeholder={modalDate ? modalDate.email : ""}
+                                    onChange={e => setEmail(e.target.value)}
+                                />
+                                <h2>Group:</h2>
+                                <div className="export">
+                                    <select onChange={e => setGroup(e.target.value)}>
+                                        <option value={modalDate ? modalDate.group_name : ""}>{modalDate ? modalDate.group_name : ""}</option>
+                                        <option value="Group 01">Group 01</option>
+                                        <option value="Group 02">Group 02</option>
+                                        <option value="Group 03">Group 03</option>
+                                    </select>
+                                </div>
+
+                                <button className="button" onClick={() => handleUpdate(modalDate.id)} type="button">Update</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div id="myModal" style={{ display: confirmation ? "block" : "none" }} className="modal">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <span onClick={() => setConfirmation(false)} className="close">&times;</span>
+                            <h2>Are you sure you want to continue?</h2>
+                        </div>
+                        <div className="modal-confirmation">
+                            <form>
+                                <button type="button" onClick={() => setConfirmation(false)} className="cancelbtn">Cancel</button>
+                                <button type="button" onClick={() => handleDeleteUser(modalDate)} className="deletebtn">Delete</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
